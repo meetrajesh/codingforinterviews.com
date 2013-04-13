a867f8ee7bdd9a749fe2a4d7feaae30306ffee67
Rajesh Kumar <rswaminathan@zynga.com>
2013-04-13 08:37:24 -0400

    Test multiple inputs

--- reverse_words.php
+++ reverse_words.php
@@ -1,34 +1,38 @@
 <?php
 
-$sentence = "Coding for Interviews contains too many gifs.";
-$reversed_sentence = reverse_words($sentence);
+$sols = array('in' => array('', 'X', 'X YZ', 'because I naughty naughty', 'The quick brown fox jumps over the lazy dogs.', 'Coding for Interviews contains too many gifs.'),
+			  'out' => array('', 'X', 'YZ X', 'naughty naughty I because', 'dogs. lazy the over jumps fox brown quick The', 'gifs. many too contains Interviews for Coding'));
 
-echo $sentence . "\n";
-echo $reversed_sentence . "\n";
-
-assert($reversed_sentence == "gifs. many too contains Interviews for Coding");
+foreach ($sols['in'] as $i => $sentence) {
+	if ($sentence) {
+		$reversed_sentence = reverse_words($sentence);
+		echo $sentence . "\n";
+		echo $reversed_sentence . "\n\n";
+		assert($reversed_sentence == $sols['out'][$i]);
+	}
+}
 
 function reverse_words($sentence) {
-	reverse_words_in_sentence($sentence);
-	reverse_sentence($sentence);
+	reverse_each_word($sentence);
+	reverse_substring($sentence);
 	return $sentence;
 }
 
-function reverse_words_in_sentence(&$sentence) {
+function reverse_each_word(&$sentence) {
 	$start = 0;
-	for ($i=0; $i < strlen($sentence); $i++) {
-		if ($sentence[$i] == ' ') {
-			reverse_word_in_sentence($sentence, $start, $i);
+	$len = strlen($sentence);
+	for ($i=0; $i <= $len; $i++) {
+		if ($i == $len || $sentence[$i] == ' ') {
+			reverse_substring($sentence, $start, $i);
 			$start = $i+1;
 		}
 	}
-	// process last word
-	reverse_word_in_sentence($sentence, $start, $i);
 }
 
-function reverse_sentence(&$sentence) {
-	for ($i=0; $i < strlen($sentence)/2; $i++) {
-		swap_chars($sentence, $i, strlen($sentence)-$i-1);
+function reverse_substring(&$sentence, $start=0, $end=null) {
+	$end = $end ?: strlen($sentence);
+	for ($i=$start; $i < ($start+$end)/2; $i++) {
+		swap_chars($sentence, $i, $start+$end-$i-1);
 	}
 }
 
@@ -36,10 +40,4 @@ function swap_chars(&$sentence, $i, $j) {
 	$tmp = $sentence[$j];
 	$sentence[$j] = $sentence[$i];
 	$sentence[$i] = $tmp;
-}
-
-function reverse_word_in_sentence(&$sentence, $start, $end) {
-	for ($i=$start; $i < ($start+$end)/2; $i++) {
-		swap_chars($sentence, $i, $start+$end-$i-1);
-	}
 }
